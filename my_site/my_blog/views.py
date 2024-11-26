e712d861d3c5291f0922ad726cdd8cdfe0afeb24
Jeronimo Dcosta <dcosta.j17@gmail.com>
2024-11-25 21:52:48 -0500

    updated build.sh to upgrade pip

--- my_site/my_blog/views.py
+++ my_site/my_blog/views.py
@@ -1,31 +1,22 @@
 from typing import Any
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.views import View
-from django.views.generic import ListView, DetailView
-from django.views.generic.base import TemplateView
-from django.views.generic.edit import FormView, CreateView
+from django.shortcuts import render, get_object_or_404 # type: ignore
+from django.http import HttpResponse # type: ignore
+from django.views import View # type: ignore
+from django.views.generic import ListView, DetailView # type: ignore
+from django.views.generic.base import TemplateView # type: ignore
+from django.views.generic.edit import FormView, CreateView # type: ignore
 
 from .models import Post
 from .forms import CommentForm
 
 
 class HomeView(TemplateView):
+    """ This class renders the Home View """
     template_name = 'my_blog/home.html'
 
-# def home(request):
-#     '''
-#         This function renders the home page of
-#         www.jeronimodcosta.com
-#     '''
-#     return render(
-#         request, 'my_blog/home.html'
-#     )
 
 class AllBlogsListView(ListView):
-    '''
-        This function renders all the blogs of the site
-    '''
+    """ This function renders all the blogs of the site """
     # List view
     # Good for fetching data from a model
     template_name = 'my_blog/all_blogs.html'
@@ -33,47 +24,6 @@ class AllBlogsListView(ListView):
     ordering = ["-date_updated"]
     context_object_name = "blogs"
 
-    # to adjust the query logic
-    # def get_queryset(self):
-    #     query = super().get_queryset()
-    #     data = query.order_by("-date_updated")
-    #     return data
-    
-
-    # Template based class view
-
-    # template_name = 'my_blog/all_blogs.html'
-
-    # def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-    #     context = super().get_context_data(**kwargs)
-    #     posts = Post.objects.all().order_by("-date_updated")
-    #     context["blogs"] = posts
-    #     return context
-    
-    
-
-    # def get(self, request):
-    #     posts = Post.objects.all().order_by("-date_updated")
-    #     return render(
-    #         request, 'my_blog/all_blogs.html',
-    #         {
-    #             "blogs" : posts,
-    #         }
-    #     )
-
-# Function view
-
-# def all_blogs(request):
-#     '''
-#         This function renders all the blogs of the site
-#     '''
-#     posts = Post.objects.all().order_by("-date_updated")
-#     return render(
-#         request, 'my_blog/all_blogs.html',
-#         {
-#             "blogs" : posts,
-#         }
-#     )
 
 class SingleBlogView(View):
     # TODO: Add Tags and File uploads to the Blog
